a6e74a917e0275776c6314b6747eff0755be5cee
theomerkaratas <theomerkaratas@gmail.com>
2026-07-14 00:39:39 +0300

    first commit

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: ["react-native-worklets/plugin"],
+    overrides: [
+      {
+        test: /[\\/]node_modules[\\/]react-native-worklets[\\/]/,
+        plugins: [
+          ["@babel/plugin-transform-private-methods", { loose: true }],
+          ["@babel/plugin-transform-class-properties", { loose: true }],
+          [
+            "@babel/plugin-transform-private-property-in-object",
+            { loose: true },
+          ],
+        ],
+      },
+    ],
+  };
+};
