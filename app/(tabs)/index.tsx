80c704770bc7fe58579f77f6bf01bf8975da1154
theomerkaratas <theomerkaratas@gmail.com>
2026-07-17 23:40:07 +0300

    feat: implement the home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,84 @@
 import "../../global.css"
-import { Text } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
 import { Link } from "expo-router";
 import { styled } from "nativewind";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import { HOME_USER, HOME_BALANCE, UPCOMING_SUBSCRIPTIONS, HOME_SUBSCRIPTIONS } from "@/constants/data";
+import { icons } from "@/constants/icons";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
+import ListHeading from "@/components/ListHeading";
+import UpcomingSubscriptionCard from "@/components/UpcomingSubscriptionCard";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import { useState } from "react";
 
 const SafeAreaView = styled(RNSafeAreaView);
 
+
 export default function App() {
+    const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<string | null>(null);
     return (
-        <SafeAreaView className="flex-1 items-center justify-center bg-background p-5">
-            <Text className="mb-6 text-2xl font-bold text-primary">
-                Welcome to Nativewind!
-            </Text>
-            <Link href="/onboarding" className="mt-2 px-6 py-3 bg-white rounded-full text-slate-900 text-lg font-semibold shadow-lg">
-                Go to Onboarding
-            </Link>
+        <SafeAreaView className="flex-1 bg-background p-5">
 
-            <Link href="/(auth)/sign-up" className="mt-4 px-6 py-3 bg-white rounded-full text-slate-900 text-lg font-semibold shadow-lg">
-                Go to Sign Up
-            </Link>
+            <FlatList
+                ListHeaderComponent={() => (
+                    <>
+                        <View className="home-header">
+                            <View className="home-user">
+                                <Image source={images.avatar} className="home-avatar" />
+                                <Text className="home-user-name">{HOME_USER.name}</Text>
+                            </View>
 
-            <Link href="/(auth)/sign-in" className="mt-4 px-6 py-3 bg-white rounded-full text-slate-900 text-lg font-semibold shadow-lg">
-                Go to Sign In
-            </Link>
+                            <TouchableOpacity className="w-12 h-12 rounded-full bg-background p-2 justify-center items-center border border-muted">
+                                <Image source={icons.add} className="w-6 h-6" />
+                            </TouchableOpacity>
+                        </View>
 
-            <Link href="/subscriptions/spotify" className="mt-6 px-6 py-3 bg-white rounded-full text-slate-900 text-lg font-semibold shadow-lg">
-                Spotify Subscription
-            </Link>
-            <Link
-                href={{
-                    pathname: "/subscriptions/[id]",
-                    params: { id: "claude" },
-                }}
-                className="mt-4 px-6 py-3 bg-white rounded-full text-slate-900 text-lg font-semibold shadow-lg"
-            >
-                Claude Subscription
-            </Link>
+                        <View className="home-balance-card">
+                            <Text className="home-balance-label">Balance</Text>
+
+                            <View className="home-balance-row">
+                                <Text className="home-balance-amount">
+                                    {formatCurrency(HOME_BALANCE.amount)}
+                                </Text>
+                                <Text className="home-balance-date">
+                                    {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+                                </Text>
+                            </View>
+                        </View>
+
+                        <View className="mb-5">
+                            <ListHeading title="Upcoming" />
+                            
+                            <FlatList
+                                data={UPCOMING_SUBSCRIPTIONS}
+                                keyExtractor={(item) => item.id}
+                                renderItem={({ item }) => <UpcomingSubscriptionCard {...item} />}
+                                horizontal
+                                showsHorizontalScrollIndicator={false}
+                                ListEmptyComponent={<Text className="home-empty-state">No upcoming renewals yet.</Text>}
+                            />
+                        </View>
+
+                        <ListHeading title="All Subscriptions" />
+                    </>
+                )}
+                data={HOME_SUBSCRIPTIONS} 
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <SubscriptionCard 
+                        {...item} 
+                        expanded={expandedSubscriptionId === item.id} 
+                        onPress={() => setExpandedSubscriptionId((currentId) => currentId === item.id ? null : item.id)} 
+                    />
+                )}
+                extraData={expandedSubscriptionId}
+                ItemSeparatorComponent={() => <View className="h-4" />}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={<Text className="home-empty-state">No subscriptions yet.</Text>}
+                contentContainerClassName="pb-30"
+            />
         </SafeAreaView>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,25 @@
 import "../global.css"
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'sans-bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'sans-light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+    'sans-regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'sans-extrabold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'sans-medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'sans-semibold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf')
+  })
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
